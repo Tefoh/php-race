8411608b53bedd43fa6ac76f1713ed8f5418a1a1
Tefoh <tefo.ha27@gmail.com>
2024-03-03 18:19:12 +0330

    feat(vehicles): set faster and slower vehicles

--- src/Console/PlayCommand.php
+++ src/Console/PlayCommand.php
@@ -9,10 +9,14 @@ class PlayCommand
     private VehicleInterface $firstPlayer;
     private VehicleInterface $secondPlayer;
 
+    private array $fasterVehicle;
+    private array $slowerVehicle;
+
 
     public function startRace($players)
     {
         $this->setPlayers($players);
+        $this->setFasterAndSlowerVehicles();
         $this->setPlayerCycles();
 
         $this->firstPlayer->run();
@@ -35,17 +39,51 @@ class PlayCommand
         $this->secondPlayer = $secondPlayer;
     }
 
-    private function setPlayerCycles()
+    private function setFasterAndSlowerVehicles()
+    {
+        $this->setFasterVehicle();
+        $this->setSlowerVehicle();
+    }
+
+    private function setFasterVehicle()
+    {
+        $this->fasterVehicle = $this->isFirstPlayerFaster()
+            ? ['player' => 1, 'vehicle' => $this->firstPlayer]
+            : ['player' => 2, 'vehicle' => $this->secondPlayer];
+    }
+
+    private function setSlowerVehicle()
+    {
+        $this->slowerVehicle = $this->isFirstPlayerSlower()
+            ? ['player' => 1, 'vehicle' => $this->firstPlayer]
+            : ['player' => 2, 'vehicle' => $this->secondPlayer];
+    }
+
+    public function isFirstPlayerFaster(): bool
     {
         $firstPlayerSpeed = $this->firstPlayer->getSpeed();
         $secondPlayerSpeed = $this->secondPlayer->getSpeed();
 
-        if ($firstPlayerSpeed > $secondPlayerSpeed) {
-            $this->firstPlayer->setCycles(100);
-            $this->secondPlayer->setCycles(100 / ($firstPlayerSpeed / $secondPlayerSpeed));
-        } else {
-            $this->secondPlayer->setCycles(100);
-            $this->firstPlayer->setCycles(100 / ($secondPlayerSpeed / $firstPlayerSpeed));
-        }
+        return $firstPlayerSpeed > $secondPlayerSpeed;
+    }
+
+    public function isFirstPlayerSlower(): bool
+    {
+        return ! $this->isFirstPlayerFaster();
+    }
+
+    private function setPlayerCycles()
+    {
+        /** @var VehicleInterface $fasterVehicle */
+        $fasterVehicle = $this->fasterVehicle['vehicle'];
+        /** @var VehicleInterface $slowerVehicle */
+        $slowerVehicle = $this->slowerVehicle['vehicle'];
+
+        $fasterVehicle->setCycles(100);
+        $slowerVehicle->setCycles(
+            100 / (
+                $fasterVehicle->getSpeed() / $slowerVehicle->getSpeed()
+            )
+        );
     }
 }
